edea442d1a5c7bcc4db449f36a1f511159ef392a
David Sorensen <teardownthismall@gmail.com>
2023-09-01 11:16:41 -0400

    fixed contributor page

--- pages/contributors/[id].js
+++ pages/contributors/[id].js
@@ -1,15 +1,31 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { Box, Container } from "@mui/system";
 import { doc, getDoc } from "firebase/firestore";
 import React from "react";
 import PageLayout from "../../components/layout/PageLayout";
 import { db } from "../../firebase";
+import NativeImage from "../../components/general/NativeImage";
 
 const page = ({ contributor }) => {
     return (
         <PageLayout name={contributor.name}>
             <Container maxWidth="md" className="section">
-                <Typography>{contributor.bio}</Typography>
+                <Grid container spacing={4}>
+                    <Grid item xs={12} md={5}>
+                        <Box className="flex flex-center flex-middle">
+                            <NativeImage
+                                src={contributor.URLs[0]}
+                                alt={contributor.name}
+                                maxSize={800}
+                            />
+                        </Box>
+                    </Grid>
+                    <Grid item>
+                        <Box className="flex flex-center flex-middle">
+                            <Typography>{contributor.bio}</Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Container>
         </PageLayout>
     );
